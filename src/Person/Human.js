1379884b5e0a2253cca178b695b211e572f4985c
Ruchir Agrawal <masterruchir.18@gmail.com>
2020-06-28 12:55:09 +0530

    Adding Class Based Component

---
+++ src/Person/Human.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+//This type of Component is also known as Class based Components
+class Human extends Component{
+    render(){
+        return(<p>I am Human Component !!</p>)
+    }
+}
+
+export default Human;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+//This type of Component is also known as React Hooks or Fucntion based Component
 const Person = () =>{
     return <p>Hello I'm Person</p>
 }
