3f4629fcc599b4f4449050f3bbc3b969912b2e39
Ruchir Agrawal <masterruchir.18@gmail.com>
2020-06-28 13:07:06 +0530

    making use of props

--- src/Person/Human.js
+++ src/Person/Human.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react'
 
 //This type of Component is also known as Class based Components
+//Like Functional Component, Class based component also has a object which is made available to it by default from react
+//We cab=n access that object by using this keyword as shown below
 class Human extends Component{
     render(){
-        return(<p>I am Human Component !!</p>)
+        return(<p>I am Human Component and my nationality is {this.props.nationality} !!</p>)
     }
 }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
 //This type of Component is also known as React Hooks or Fucntion based Component
-const Person = () =>{
-    return <p>Hello I'm Person</p>
+//Each component has a props object which is made available to it by react itself
+//Although we can give it any name, however props is preffered as it is universally used and increases code readability
+const Person = (props) =>{
+return <p>Hi, My name is {props.name} and I'm {props.age} years old !!</p>
 }
 
 export default Person;
