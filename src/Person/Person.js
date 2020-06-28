159eddc0e504ce9847c1f9dc53aa71d82345e5ed
Ruchir Agrawal <masterruchir.18@gmail.com>
2020-06-28 13:35:53 +0530

    typo error

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 //This type of Component is also known as React Hooks or Fucntion based Component
 //Each component has a props object which is made available to it by react itself
-//Although we can give it any name, however props is preffered as it is universally used and increases code readability
+//Although we can give it any name, however props is preferred as it is universally used and increases code readability
 const Person = (props) =>{
 return <p>Hi, My name is {props.name} and I'm {props.age} years old !!</p>
 }
