5ae05813470dfb4e371af2becba82b027d234794
Ruchir Agrawal <masterruchir.18@gmail.com>
2020-07-05 00:35:05 +0530

    Functional Component demonstrating the useState Hook

--- src/App.js
+++ src/App.js
@@ -1,45 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
 import Human from './Person/Human'
 
-class App extends Component {
-  state ={
+//Functional Component demonstrating the useState Hook
+const app = (props) => {
+
+  const [personState, setPersonState] = useState({
     persons: [
       {name: 'Manjeet', age: 28},
       {name: 'Max', age: 25}
     ]
-  }
+  })
 
   //Changing the state on button click
-  //setState is the method provided to us by Component class
-  //This method with merge the state provided to it with the existing state.
-  buttonClickHandler = () =>{
-    this.setState({
+  const buttonClickHandler = () =>{
+    setPersonState({
       persons: [
         {name: 'Rajesh', age:31},
         {name: 'Priya', age: 27}
       ]
     })
   }
+console.log(personState)
 
-
-  render() {
-    return (
+  return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <br></br>
-        <button onClick={this.buttonClickHandler}>Switch Name</button>
-        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
-        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}/>
+        <button onClick = {buttonClickHandler}>Switch Name</button>
+        <Person name = {personState.persons[0].name} age = {personState.persons[0].age} />
+        <Person name = {personState.persons[1].name} age = {personState.persons[1].age}/>
         <Human nationality = 'African'/>
       </div>
     );
-  }
 }
 
-export default App;
+export default app;
